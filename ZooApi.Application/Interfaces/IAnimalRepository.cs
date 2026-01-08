6e707cb89c96681bf5554b4b664ab54b162fb8d3
skrrrwwwwwww <shikarevivan2004@gmail.com>
2026-01-09 01:28:30 +0300

    Add: Aplication layer interfaces

---
+++ ZooApi.Application/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ZooApi.Application.Interfaces;
+
+public interface IAnimalRepository
+{
+    Task<Animal?> GetByIdAsync(int id);
+    Task<List<Animal>> GetAllAsync();
+    Task<int> AddAsync(Animal animal);
+    Task UpdateAsync(Animal animal);
+    Task DeleteAsync(int id);
+}
