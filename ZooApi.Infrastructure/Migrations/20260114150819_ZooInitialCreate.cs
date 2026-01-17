d1c11c407c4b0ee06d28becc1b9f58f619c6dde3
skrrrwwwwwww <shikarevivan2004@gmail.com>
2026-01-17 18:36:20 +0300

    ZooApi

---
+++ ZooApi.Infrastructure/Migrations/20260114150819_ZooInitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ZooApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ZooInitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Species = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Energy = table.Column<int>(type: "integer", nullable: false, defaultValue: 100)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
