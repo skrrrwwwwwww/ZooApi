884d6fc5dfd99febca6931c78e7881383ba06af8
skrrrwwwwwww <shikarevivan2004@gmail.com>
2026-02-02 18:48:07 +0300

    refactor: add primary constructor of EmailService

--- ZooApi.Application/Services/EmailService.cs
+++ ZooApi.Application/Services/EmailService.cs
@@ -6,32 +6,25 @@ using ZooApi.Application.Interfaces;
 
 namespace ZooApi.Application.Services;
 
-public class EmailService : IEmailService
+public class EmailService(IConfiguration configuration) : IEmailService
 {
-    private readonly IConfiguration _configuration;
-
-    public EmailService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
     public async Task SendEmailAsync(string toEmail, string subject, string body)
     {
         var email = new MimeMessage();
-        email.From.Add(MailboxAddress.Parse(_configuration["MailSettings:Username"]));
+        email.From.Add(MailboxAddress.Parse(configuration["MailSettings:Username"]));
         email.To.Add(MailboxAddress.Parse(toEmail));
         email.Subject = subject;
         email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
         
         using var smtp = new SmtpClient();
         
-        var port = int.TryParse(_configuration["MailSettings:Port"], out var p) ? p : 587;
+        var port = int.TryParse(configuration["MailSettings:Port"], out var p) ? p : 587;
         
         await smtp.ConnectAsync(
-            _configuration["MailSettings:Host"],
-            int.Parse(_configuration["MailSettings:Port"]), 
+            configuration["MailSettings:Host"],
+            int.Parse(configuration["MailSettings:Port"]), 
             SecureSocketOptions.StartTls);
-        await smtp.AuthenticateAsync(_configuration["MailSettings:Username"], _configuration["MailSettings:Password"]);
+        await smtp.AuthenticateAsync(configuration["MailSettings:Username"], configuration["MailSettings:Password"]);
         await smtp.SendAsync(email);
         await smtp.DisconnectAsync(true);
     }
